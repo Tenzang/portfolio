b3c81e62974941f1a8b27bc5907afafbe72305c1
Loden Gendun <gendun.loden@gmail.com>
2024-08-11 13:07:19 +1000

    feat: add profile picture to intro

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+import { getImageUrl } from "~/util"
+</script>
 <template>
     <Section heading="Introduction" class="backdrop">
+        <img :src="getImageUrl('profile.png')" alt="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at magnam tempore ipsam cupiditate. Dolor, quasi
         amet corrupti culpa nostrum in molestiae. Ea reprehenderit doloribus nisi quod sequi perferendis in?
     </Section>
 </template>
+
+<style scoped lang="scss">
+img {
+    height: 50vw;
+    width: 50vw;
+    border-radius: 100%;
+    border: 1px solid $highlight;
+    box-shadow: 0 0 2rem -1rem $highlight;
+    margin: 0 auto;
+}
+</style>
--- util/index.ts
+++ util/index.ts
@@ -1,7 +1,15 @@
-export function getIconUrl(fileName: string) {
+function getPublicAssetUrl(filePath: string) {
   const pathPrefix = import.meta.env.VITE_PATH_PREFIX;
-  let output = `/icons/${fileName}.svg`;
+  let output = filePath;
 
   if (!pathPrefix) return output;
   return `/${pathPrefix}${output}`;
+}
+
+export function getIconUrl(fileName: string) {
+  return getPublicAssetUrl(`/icons/${fileName}.svg`);
+}
+
+export function getImageUrl(fileName: string) {
+  return getPublicAssetUrl(`/images/${fileName}`);
 }
